55da5982aabdf3192b58ccffdca05fa888579cec
Sofia Silva <up201505628@fe.up.pt>
2019-01-03 19:15:02 +0000

    most profitable + active user

--- java/src/AgendaViral/Interface.java
+++ java/src/AgendaViral/Interface.java
@@ -161,58 +161,18 @@ public class Interface {
 			break;
 		}
 	}
-	
-	public void mostActiveMenu() {
-		// TODO Auto-generated method stub
-		
-	}
-
-	public void mostProfitableMenu() {
-		// TODO Auto-generated method stub
-		
-	}
-
-	public void mostPopularMenu() {
-		Event event = agenda.mostPopularEvent();
-		
-		System.out.println(" ------------------------------------------- ");
-		System.out.println("|             Most Popular Event            |");
-		System.out.println(" ------------------------------------------- ");
-
-		System.out.println(" Id: " + event.getID());
-		System.out.println(" Title: " + event.getTitle());
-		System.out.println(" Category: " + event.getCategory());
-		System.out.println(" City: " + event.getCity());
-		System.out.println(" Date: from " + event.getDateStart().day + "/" + event.getDateStart().month + "/"
-				+ event.getDateStart().year + " to " + event.getDateEnd().day + "/" + event.getDateEnd().month + "/"
-				+ event.getDateEnd().year);
-		System.out.println(" Price: " + event.getPrice() + " euros");
-		System.out.println(" Total Tickets: " + event.getTotalTickets() + " | Sold Tickets: " + event.getSoldTickets());
-		System.out.println(" Descriprion: " + event.getDescription());
-
-		System.out.println(" ------------------------------------------- ");
-
-		System.out.println("Press Enter key to continue...");
-        try
-        {
-            System.in.read();
-        }  
-        catch(Exception e)
-        {}
-		mainMenuAdmin();
-	}
 
 	public void addEventMenu() {
 		System.out.println(" ------------------------------------------- ");
 		System.out.println("|                 Add Event                 |");
 		System.out.println(" ------------------------------------------- ");
-		
+
 		System.out.print(" > Title: ");
 		String title = scanner.nextLine();
-		
+
 		System.out.print(" > Category: ");
 		String category = scanner.nextLine();
-		
+
 		System.out.print(" > Starting Date[dd/mm/yy]: ");
 		String startDate = scanner.nextLine();
 		String[] part1 = startDate.split("/");
@@ -220,7 +180,7 @@ public class Interface {
 		int day1 = Integer.parseInt(part1[0]);
 		int month1 = Integer.parseInt(part1[1]);
 		int year1 = Integer.parseInt(part1[2]);
-		
+
 		System.out.print(" > Ending Date[dd/mm/yy]: ");
 		String endDate = scanner.nextLine();
 		String[] part2 = endDate.split("/");
@@ -228,26 +188,26 @@ public class Interface {
 		int day2 = Integer.parseInt(part2[0]);
 		int month2 = Integer.parseInt(part2[1]);
 		int year2 = Integer.parseInt(part2[2]);
-		
+
 		System.out.print(" > Description: ");
 		String description = scanner.nextLine();
-		
+
 		System.out.print(" > Price: ");
 		int price = scanner.nextInt();
 		scanner.nextLine();
-		
+
 		System.out.print(" > City: ");
 		String city = scanner.nextLine();
-		
+
 		System.out.print(" > Total Tickets: ");
 		int tickets = scanner.nextInt();
 		scanner.nextLine();
-		
+
 		Event event = new Event(title, category, new Event.Date(day1, month1, year1),
 				new Event.Date(day2, month2, year2), description, price, city, tickets);
-		
+
 		agenda.addEvent(event);
-		
+
 		mainMenuAdmin();
 	}
 
@@ -314,6 +274,82 @@ public class Interface {
 		else if (action.equals("R"))
 			agenda.rejectProposedEvent(event);
 
+		mainMenuAdmin();
+	}
+
+	public void mostPopularMenu() {
+		Event event = agenda.mostPopularEvent();
+
+		System.out.println(" ------------------------------------------- ");
+		System.out.println("|             Most Popular Event            |");
+		System.out.println(" ------------------------------------------- ");
+
+		System.out.println(" Id: " + event.getID());
+		System.out.println(" Title: " + event.getTitle());
+		System.out.println(" Category: " + event.getCategory());
+		System.out.println(" City: " + event.getCity());
+		System.out.println(" Date: from " + event.getDateStart().day + "/" + event.getDateStart().month + "/"
+				+ event.getDateStart().year + " to " + event.getDateEnd().day + "/" + event.getDateEnd().month + "/"
+				+ event.getDateEnd().year);
+		System.out.println(" Price: " + event.getPrice() + " euros");
+		System.out.println(" Total Tickets: " + event.getTotalTickets() + " | Sold Tickets: " + event.getSoldTickets());
+		System.out.println(" Descriprion: " + event.getDescription());
+
+		System.out.println(" ------------------------------------------- ");
+
+		System.out.println(" > Press Enter to continue...");
+		try {
+			System.in.read();
+		} catch (Exception e) {
+		}
+		mainMenuAdmin();
+	}
+
+	public void mostProfitableMenu() {
+		Event event = agenda.mostProfitableEvent();
+
+		System.out.println(" ------------------------------------------- ");
+		System.out.println("|           Most Profitable Event           |");
+		System.out.println(" ------------------------------------------- ");
+
+		System.out.println(" Id: " + event.getID());
+		System.out.println(" Title: " + event.getTitle());
+		System.out.println(" Category: " + event.getCategory());
+		System.out.println(" City: " + event.getCity());
+		System.out.println(" Date: from " + event.getDateStart().day + "/" + event.getDateStart().month + "/"
+				+ event.getDateStart().year + " to " + event.getDateEnd().day + "/" + event.getDateEnd().month + "/"
+				+ event.getDateEnd().year);
+		System.out.println(" Price: " + event.getPrice() + " euros");
+		System.out.println(" Total Tickets: " + event.getTotalTickets() + " | Sold Tickets: " + event.getSoldTickets());
+		System.out.println(" Descriprion: " + event.getDescription());
+
+		System.out.println(" ------------------------------------------- ");
+
+		System.out.println(" > Press Enter to continue...");
+		try {
+			System.in.read();
+		} catch (Exception e) {
+		}
+		mainMenuAdmin();
+	}
+
+	public void mostActiveMenu() {
+		User user = agenda.mostActiveUser();
+
+		System.out.println(" ------------------------------------------- ");
+		System.out.println("|               Most Active User            |");
+		System.out.println(" ------------------------------------------- ");
+
+		System.out.println(" Email: " + user.getEmail());
+		System.out.println(" No. Bought Tickets: " + user.getTicketsBought());
+
+		System.out.println(" ------------------------------------------- ");
+
+		System.out.println(" > Press Enter to continue...");
+		try {
+			System.in.read();
+		} catch (Exception e) {
+		}
 		mainMenuAdmin();
 	}
 
